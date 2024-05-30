5812730fc123f7c1a53528b844737d96039731b2
AbdoXm <treated.contact@gmail.com>
2024-05-30 16:25:08 +0100

    Initial commit

---
+++ src/main/java/com/abdessamad/karimi/blockchain_tp_abdo/blockchain/Block.java
@@ -0,0 +1,54 @@
+package com.abdessamad.karimi.blockchain_tp_abdo.blockchain;
+
+import com.abdessamad.karimi.blockchain_tp_abdo.utils.HashUtil;
+
+import java.util.Date;
+public class Block {
+    private int index;
+    private long timestamp;
+    private String previousHash;
+    private String currentHash;
+    private String data;
+    private int nonce;
+
+    public Block(int index, String previousHash, String data) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = new Date().getTime();
+        this.currentHash = calculateHash();
+    }
+
+    public String calculateHash() {
+        return HashUtil.applySha256(index + Long.toString(timestamp) + previousHash + data + nonce);
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!currentHash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            currentHash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + currentHash);
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public String getCurrentHash() {
+        return currentHash;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
---
+++ src/test/java/com/abdessamad/karimi/blockchain_tp_abdo/BlockchainTpAbdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdessamad.karimi.blockchain_tp_abdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainTpAbdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
